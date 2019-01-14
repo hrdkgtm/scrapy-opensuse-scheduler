3fb8cb7c4e182545f416fbe5ea0a651a5dff0299
Hardika Gutama <hg@s8i.io>
2019-01-14 17:41:48 +0700

    [Improvement] how it behaves on the beginning/end of the month. Recudes bloat

--- crawl-current.sh
+++ crawl-current.sh
@@ -8,6 +8,9 @@
 THIS_YEAR=`date +'%Y' -d 'now'`
 PREV_YEAR=$THIS_YEAR
 NEXT_YEAR=$THIS_YEAR
+
+THIS_DAY=`date +'%d' -d 'now'`
+
 THIS_MONTH=`date +'%m' -d 'now'`
 PREV_MONTH=`date +'%m' -d 'last month'`
 NEXT_MONTH=`date +'%m' -d 'next month'`
@@ -32,13 +35,27 @@ fi
 
 #Functions
 get_mail () {
-    curl http://$SCRAPYD_SERVER/schedule.json -d project=$PROJECT_NAME -d spider=$SPIDER -d setting=FEED_URI="$DATADIR/$1/$2/$3.json" -d category=$1 -d date="$2-$3" > /dev/null
-    #echo $1 $2 $3
+    #curl http://$SCRAPYD_SERVER/schedule.json -d project=$PROJECT_NAME -d spider=$SPIDER -d setting=FEED_URI="$DATADIR/$1/$2/$3.json" -d category=$1 -d date="$2-$3" > /dev/null
+    echo $1 $2 $3
 }
-make_request () {
+make_request_prev () {
     for MAILNAME in "${MAIL_LIST[@]}"
     do
         get_mail $MAILNAME $PREV_YEAR $PREV_MONTH
+        get_mail $MAILNAME $THIS_YEAR $THIS_MONTH
+    done
+}
+
+make_request_current () {
+    for MAILNAME in "${MAIL_LIST[@]}"
+    do
+        get_mail $MAILNAME $THIS_YEAR $THIS_MONTH
+    done
+}
+
+make_request_next () {
+    for MAILNAME in "${MAIL_LIST[@]}"
+    do
         get_mail $MAILNAME $THIS_YEAR $THIS_MONTH
         get_mail $MAILNAME $NEXT_YEAR $NEXT_MONTH
     done
@@ -47,7 +64,16 @@ make_request () {
 # The forever loop until it fails, sleep for 60s
 while true
 do
-    make_request
+    if [ $THIS_DAY -eq '1' ]
+    then
+        make_request_prev
+    elif [ $THIS_DAY -gt '27' ]
+    then
+        make_request_next
+    else
+        make_request_current
+    fi
+    
     if [ $? -eq 0 ]
     then
         sleep 60
